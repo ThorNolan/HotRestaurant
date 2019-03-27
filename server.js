a7c922f2fbb09b614b56fcf6311317fd1e65d4af
kchoi123 <k3vinch0i@gmail.com>
2019-03-27 13:29:32 -0700

    new updates for skel from team

---
+++ server.js
@@ -0,0 +1,64 @@
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var tables = [];
+var waitlist = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/viewtables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+app.get("/api/tables/:table", function(req, res) {
+  var table = req.params.table;
+
+  console.log(table);
+
+  for (var i = 0; i < tables.length; i++) {
+    if (table === tables[i].resName) {
+      return res.json(tables[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+app.post("/api/tables", function(req, res) {
+  var newTable = req.body;
+
+  newTable.id = newTable.resName.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newTable);
+
+  if (tables.length < 5) {
+    tables.push(newTable);
+  } else {
+    waitlist.push(newTable);
+  }
+
+  tables.push(newTable);
+
+  res.json(newTable);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
